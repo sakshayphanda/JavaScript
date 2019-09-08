dde32d44814d20f3975b14f1283264deeec9bd11
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-08 19:53:33 +0530

    weakset vs set

---
+++ JS Advance topics/SetsVSWeakSet.js
@@ -0,0 +1,13 @@
+// SET vs WEAKSET
+
+let obj = {a: 1};
+
+const set = new Set();
+const weakSet = new WeakSet();
+set.add(obj);
+weakSet.add(obj);
+console.log(set, weakSet.has(obj));
+
+obj = null;
+console.log(set, weakSet.has(obj));
+
