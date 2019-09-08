801cb6fc63b73d7aefb234f6c21d114f1f9dd068
sakshay phanda <sakshay@sakshays-MacBook-Pro.local>
2019-09-08 18:57:11 +0530

    coercion in js

---
+++ JS topics/Coercion.js
@@ -0,0 +1,15 @@
+
+// Coercion
+// Its when js predicts things on its own 
+//Javascript is smart enough to determine some of the things on its own.
+
+
+console.log(5 - '5'); // both considered as numbers
+console.log(5 + '5'); // both considered as strings
+// {} // true
+// [] // true
+// '' // false
+
+
+//false = 0
+//true = 1
