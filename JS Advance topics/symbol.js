f27f634a7734d58d9e095a77362e0de16743c9e4
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-10 11:54:34 +0530

    symbol

---
+++ JS Advance topics/symbol.js
@@ -0,0 +1,11 @@
+// Symbol
+
+/**
+ * to generate a unique value 
+ * can be used in objects to create a unique uid
+ */
+
+const symbol = Symbol();
+const symbol2 = Symbol();
+
+console.log(symbol2 === symbol);
