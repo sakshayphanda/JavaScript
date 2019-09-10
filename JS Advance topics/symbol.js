f93344d2f965a556a9468b7578a9255784c94bd7
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-10 12:03:14 +0530

    symbol updated

--- JS Advance topics/symbol.js
+++ JS Advance topics/symbol.js
@@ -9,3 +9,11 @@ const symbol = Symbol();
 const symbol2 = Symbol();
 
 console.log(symbol2 === symbol);
+
+
+// to create a global symbol we use symbol. for 
+const symbol3 = Symbol.for('a');
+const symbol4 = Symbol.for('a');
+
+
+console.log(symbol3 === symbol4);
