d7b627d4d162732de60641ef3cb5718ee4dd8a5a
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-13 10:39:21 +0530

    callback in jas

--- JS Advance topics/callback.js
+++ JS Advance topics/callback.js
@@ -1,12 +1,15 @@
-function test (callback) {
-    const str = 'abc';
+function onClick (callback) {
+    const str = 'Button is clicked';
 
     if (typeof callback === "function") {
     callback(str);
-
     }
 }
 
-test((str) => {
+
+let clickFunction = (str) => {
     console.log(str);
-});
+};
+
+onClick(clickFunction)
+
