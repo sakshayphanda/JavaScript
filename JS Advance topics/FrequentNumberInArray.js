71f1943f43703abe0c7f4aa4f0684952b08af207
sakshay phanda <sakshayphanda03@gmail.com>
2019-09-17 10:42:56 +0530

    optimized Frequent number

--- JS Advance topics/FrequentNumberInArray.js
+++ JS Advance topics/FrequentNumberInArray.js
@@ -1,32 +1,31 @@
-arr = [1, 3, 5, 6, 3, 6 , 6, 6,6 ];
+arr = [1, 3, 5, 3, 3, 3 , 3, 6,6 ];
 
 function mostFrequentNumberInArray(arr) {
    // const tempArr = [...arr];
     const obj = {};
+    let max = arr[0];
     arr.forEach(
         item => {
-            obj[item] = 0;
-        }
-    );
+            obj[item] = obj[item] === undefined ? 0 : obj[item];
 
-    arr.forEach(
-        item => {
             if(item in obj) {
                 obj[item] += 1;
             } 
+
+            if(obj[item] > max ) {
+                max = item;
+            }
         }
      );
 
-   // console.log(obj);
-
-    let max = Object.keys(obj)[0];
-    Object.keys(obj).forEach(
-        key => {
-            if(obj[key] > max ) {
-                max = key;
-            }
-        }
-    );
+    // let max = Object.keys(obj)[0];
+    // Object.keys(obj).forEach(
+    //     key => {
+    //         if(obj[key] > max ) {
+    //             max = key;
+    //         }
+    //     }
+    // );
 
     return max;
 }
@@ -37,4 +36,4 @@ const result = mostFrequentNumberInArray(arr);
 console.log(result);
 // console.log(0o12 === 0XA);
 
-console.log(013);// 8
+// console.log(010);// 8
