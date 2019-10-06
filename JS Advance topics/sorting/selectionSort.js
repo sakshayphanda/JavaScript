ca5adf430f67e144183a309e3c3f5803a34a2390
sakshay phanda <sakshayphanda03@gmail.com>
2019-10-06 13:02:46 +0530

    folder structure changed

--- JS Advance topics/sorting/selectionSort.js
+++ JS Advance topics/sorting/selectionSort.js
@@ -10,18 +10,19 @@
     let arr = [2,4,5,1, -2, 8,3];
     let temp;
 
- for(i = 0; i < arr.length; i++) {
-    minIndex = i;
 
-     for( j = i + 1; j< arr.length; j++) {
-        if(arr[minIndex] > arr[j] ) {
-            minIndex = j;
+    for(let i = 0; i < arr.length; i++) {
+        minIndex = i;
+        for(let j = i + 1; j <  arr.length; j++) {
+         //   console.log(arr[j]);
+            if(arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
         }
-     }
+       // console.log(minIndex, ': ' , arr[minIndex]);
         temp = arr[i];
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
- }
+    }
 
-
- console.log(arr);
+    console.log(arr);
