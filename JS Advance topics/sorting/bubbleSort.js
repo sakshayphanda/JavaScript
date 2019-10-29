8faee2d71f005bcbd96f9a30b69f4f54469c36c1
sakshay phanda <sakshayphanda03@gmail.com>
2019-10-29 14:40:18 +0530

    removed useless code

--- JS Advance topics/sorting/bubbleSort.js
+++ JS Advance topics/sorting/bubbleSort.js
@@ -24,16 +24,4 @@ for (let i = 0; i < maxArrIndex; i++) {
     }
 }
 
-
-// for (let i = 0; i < arr.length; i++) {
-//     for(let j = i+1; j< arr.length; j++) {
-//         if(arr[i]> arr[j]) {
-//             temp = arr[i];
-//             arr[i] = arr[j];
-//             arr[j] = temp;
-//         }
-//     }
-// }
-
-
 console.log(arr);
