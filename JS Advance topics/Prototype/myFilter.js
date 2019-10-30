b3a9920e659b4010d1420b80c29fe7db6140c551
sakshay phanda <sakshayphanda03@gmail.com>
2019-10-30 10:31:58 +0530

    minor chasnges

--- JS Advance topics/Prototype/myFilter.js
+++ JS Advance topics/Prototype/myFilter.js
@@ -1,19 +1,18 @@
 let arr = [4, 5, 2, 4, 2, 3];
 
-Array.prototype.myFilter = function(func) {
+Array.prototype.myFilter = function (func) {
     let newArray = [];
-    for(let i = 0; i< this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         let state = func(this[i], i, this);
-        if(state) {
+        if (state) {
             newArray.push(this[i]);
         }
     }
     return newArray;
 }
 
-
 arr = arr.myFilter((item) => {
-    if(item < 1) {
+    if (item < 1) {
         return true;
     }
 });
