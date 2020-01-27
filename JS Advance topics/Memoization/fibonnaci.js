46fa634de8933da954c7650a54356d02bf3a85ff
sakshay phanda <sakshayphanda03@gmail.com>
2020-01-27 10:54:31 +0530

    fix

--- JS Advance topics/Memoization/fibonnaci.js
+++ JS Advance topics/Memoization/fibonnaci.js
@@ -5,38 +5,27 @@
  */
 
 
-function fibonacci(n, previousValues = []) {
-
-    if(previousValues[n] != null) {
-        return previousValues[n];
-    }
-    // console.log('n:', n);
+function fibonacci(n, previousValues = [], type) { // 5 , []
+   // console.log(type, '-', 'n:', n, previousValues[n]);
+    // if(previousValues[n] != undefined) {
+    //     return previousValues[n];
+    // }
 
     let result;
     if(n<=2) {
         result = 1;
     } 
     else {
-       // console.log(fibonacci(n-1) + fibonacci(n-2));
-        result = fibonacci(n-1, previousValues) + fibonacci(n-2, previousValues);
-
-        //fib(4)
-        // fib(3) + fib(2)        
-        // (fib(2) + fib(1)) + 1
-        // 1 + 1 + 1 = 3
-
-
+        result = fibonacci(n-1, previousValues, 'II') + fibonacci(n-2, previousValues, 'III');
     }
 
     previousValues[n] = result;
     console.log(n+': ' , previousValues[n]);
-  //  console.log(previousValues);
-
     return result;
 
 }
 
-console.log(fibonacci(5));
+fibonacci(10, [], 'I');
 
 // 0 1 2 3 4 6 6 7  8  9  10
 // 0 1 1 2 3 5 8 13 21 34 55
