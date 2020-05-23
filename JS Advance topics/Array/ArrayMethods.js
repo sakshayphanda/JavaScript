0397b475920879c5fe53b215e8fa4adfd48c7d8c
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-05-23 17:20:17 +0530

    update

--- JS Advance topics/Array/ArrayMethods.js
+++ JS Advance topics/Array/ArrayMethods.js
@@ -6,6 +6,10 @@
  * PUSH
  * POP
  * CONCAT
+        const array1 = ['a', 'b', 'c'];
+        const array2 = ['d', 'e', 'f'];
+        const array3 = array1.concat(array2);
+        console.log(array3); // ["a", "b", "c", "d", "e", "f"]
  * LENGTH
  * INCLUDES
  * slice
