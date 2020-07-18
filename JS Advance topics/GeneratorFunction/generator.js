cfabac457d0b5d451d899a74a301e8ec687fac9c
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-07-18 12:29:35 +0530

    update

---
+++ JS Advance topics/GeneratorFunction/generator.js
@@ -0,0 +1,15 @@
+function * generatorFunction() { // Line 1
+ //   console.log('This will be executed first.');
+    yield 'Hello, ';   // Line 2
+   // console.log('I will be printed after the pause');  
+    yield 'World!';
+  }
+  const generatorObject = generatorFunction(); // Line 3
+  console.log(generatorObject.next()); // Line 4
+   console.log(generatorObject.next()); // Line 5
+   console.log(generatorObject.next()); // Line 6
+
+
+// for(obj of generatorObject) {
+//     console.log(obj);
+// }
