31fbb69c10bac9f7e9bd87e6d25f4acf109b6caf
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-07-19 02:44:27 +0530

    fix

--- JS Advance topics/Generics in TypeScript/class-generics.ts
+++ JS Advance topics/Generics in TypeScript/class-generics.ts
@@ -5,7 +5,7 @@ class Manager extends Person{
 
 }
 
-class Admin extends Manager{
+class Admin extends Person{
 
 }
 
@@ -16,11 +16,13 @@ class Random {
 
 const a = new Admin();
 const m = new Manager();
+const r = new Random();
 
-function echoGeneric<T extends Person>(arg: T): T {
+function echoGeneric2<T extends Person>(arg: T): T {
     const name = arg.name;
     return arg;
 }
 
-let temp4 = echoGeneric(a);//temp2 is of type string
-let temp5 = echoGeneric(m); // temp3 is of type number
+let temp4 = echoGeneric2(a);//temp2 is of type string
+let temp5 = echoGeneric2(m); // temp3 is of type number
+let temp6 = echoGeneric2(r); //error because it doesnt extend Person class
---
+++ JS Advance topics/Generics in TypeScript/class-generics2.ts
@@ -0,0 +1,19 @@
+class Calculator<T> {
+    items: T[];
+    constructor(private a: T) {
+        this.a = a;
+    }
+
+    push(val): T[] {
+        this.items.push();
+        return this.items;
+    }
+}
+
+
+const calc = new Calculator(5);
+const calcString = new Calculator<string>('5');
+
+console.log(calc.push(10));
+console.log(calcString.push('15'));
+
