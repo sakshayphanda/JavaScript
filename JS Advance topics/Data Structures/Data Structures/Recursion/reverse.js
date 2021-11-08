216262fc3aa6af45c36ce7325a40b23f30547faa
Sakshay Phanda <sakshayphanda03@gmail.com>
2021-11-08 16:31:09 +0530

    this updated

--- JS Advance topics/Data Structures/Data Structures/Recursion/reverse.js
+++ JS Advance topics/Data Structures/Data Structures/Recursion/reverse.js
@@ -1,16 +1,25 @@
-function reverse(str){
-    // add whatever parameters you deem necessary - good luck!
-    //1. base condition
-    let newArr = [];
-    if(!str) return str;
-      const arr = str.toString().split('');
-      newArr.push(arr[arr.length - 1]); // ['d']
-      console.log(str, arr, newArr, arr.length - 1);
-      newArr = newArr.concat(reverse(arr.slice(0, arr.length - 1).join('')));  
-      return newArr.join('');
-  
-  }
+function reverse(str) {
+	//1. base condition
+	let newArr = [];
+	if (!str) return str;
+	const arr = str.toString().split('');
+	newArr.push(arr[arr.length - 1]); // ['d']
+	console.log(str, arr, newArr, arr.length - 1);
+	newArr = newArr.concat(reverse(arr.slice(0, arr.length - 1).join('')));
+	return newArr.join('');
+}
 
-  // reverse('abcd') --> ['d'].concat(['c'].concat(['b'].concat(['a'])));
+// reverse('abcd') --> ['d'].concat(['c'].concat(['b'].concat(['a'])));
 
-  console.log(reverse('abcd'));
+// console.log(reverse('abcd'));
+
+//  better and my own version
+function reverseStr(str) {
+	if (!str) return str;
+	return ''.concat(
+		str[str.length - 1],
+		reverseStr(str.slice(0, str.length - 1))
+	);
+}
+
+console.log(reverseStr('abcd'));
