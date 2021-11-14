388f620740ab03111e1a4865b274e767cc6dfa7e
Sakshay Phanda <sakshayphanda03@gmail.com>
2021-11-14 15:53:46 +0530

    fix

--- JS Advance topics/Prototype/myFilter.js
+++ JS Advance topics/Prototype/myFilter.js
@@ -1,9 +1,9 @@
 let arr = [4, 5, 2, 4, 2, 3];
 
-Array.prototype.myFilter = function (func) {
+Array.prototype.myFilter = function (callback) {
     let newArray = [];
     for (let i = 0; i < this.length; i++) {
-        let state = func(this[i], i, this);
+        let state = callback(this[i], i, this);
         if (state) {
             newArray.push(this[i]);
         }
